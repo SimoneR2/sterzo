f5a8283cb2e4f170a1a34d076bec676b10d71d75
SimoneR2 <simone.righetti97@gmail.com>
2015-12-06 22:37:52 +0100

    Eliminata rampa
    1) eliminata rampa di raggiungimento angolo desiderato perchè inutile
    2) migliorato sistema gestione angolo massimo

--- newmain.c
+++ newmain.c
@@ -18,7 +18,7 @@
 
 #define _XTAL_FREQ 16000000
 #define Steering_info 0b10000000000000000000000000000
-#define Steering_change 0b00000000000000000000000000011
+#define Steering_change 0b00000000000000000000000000110
 #define emergency 0b00000000000000000000000000001
 
 void configurazione_iniziale(void);
@@ -28,12 +28,11 @@ CANmessage msg;
 bit remote_frame = 0;
 
 //da variare per limitare l'angolo di sterzata
-unsigned int limiteDx = 0;
-unsigned int limiteSx = 180;
+unsigned int limiteDx = 120;
 
 char currentSteering = 180;
 char pastSteering = 1;
-unsigned char theorySteering = 0;
+char theorySteering = 0;
 unsigned long counter = 0;
 unsigned long id = 0;
 unsigned long timeCounter = 0; //1 = 10mS
@@ -82,18 +81,13 @@ __interrupt(low_priority) void ISR_bassa(void) {
             }
             if (msg.identifier == Steering_info) {
                 data_array[0] = theorySteering;
-                if (theorySteering <= limiteDx) {
-                    theorySteering = limiteDx;
-                }
-                if (theorySteering >= limiteSx) {
-                    theorySteering = limiteSx;
-                }
+                
             }
             if (msg.identifier == Steering_change) {
                 //pastSteering = currentSteering;
                 theorySteering = msg.data[0];
                 currentSteering = theorySteering + calibration; //aggiunta calibrazione
-               // previousTimeCounter = timeCounter;
+                currentSteering = (limiteDx*currentSteering)/180;
             }
         }
         PIR3bits.RXB0IF = 0;
@@ -109,42 +103,46 @@ __interrupt(low_priority) void ISR_bassa(void) {
 
 int main(void) {
     configurazione_iniziale();
-    //debug sequence ---
-    PORTC = 0xFF; //
-    delay_ms(100); //
-    PORTC = 0x00; //
-    delay_ms(100); //
-    //------------------
-    TMR0H = 0xdd;
-    TMR0L = 0xa0;
+    timer = (((0 * 700) / 90) + 800) *2;
+    Ton = 65536 - timer;
+    Toff = 20000 - (timer / 2);
+    Toff = (65536 - (Toff * 2));
+    WriteTimer0(Ton);
     T0CONbits.TMR0ON = 1;
     Toff = 0x4588;
     while (1) {
         calibrazione();
-        if (timeCounter - previousTimeCounter > 1) {
-            errore = pastSteering - currentSteering;
-            errore = abs(errore); //modulo di "errore"
-            if (errore > 2) {
-                correzione = ((errore / 15)*(errore / 15)); //potenza di due (la libreria dà errore)
-                if (correzione < 1) {
-                    duty_cycle = currentSteering;
-                } else if ((pastSteering - currentSteering) < 0) { //sterzo verso Dx
-                    duty_cycle = duty_cycle + correzione;
-                } else if ((pastSteering - currentSteering) > 0) { //sterzo verso Sx
-                    duty_cycle = duty_cycle - correzione;
-                }
-            } else {
-                duty_cycle = currentSteering;
-            }
-            if ((pastSteering - currentSteering) < 0) {
-                duty_cycle = duty_cycle + correzione;
-            }
-            if ((pastSteering - currentSteering) > 0) {
-                duty_cycle = duty_cycle - correzione;
-            }
-            pastSteering = duty_cycle;
-            previousTimeCounter = timeCounter;
-        }
+        duty_cycle = currentSteering;
+//        if (timeCounter - previousTimeCounter > 5) {
+//            errore = pastSteering - currentSteering;
+//            errore = abs(errore); //modulo di "errore"
+//            if (errore > 2) {
+//                correzione = ((errore / 15)*(errore / 15)); //potenza di due (la libreria dà errore)
+//                if (correzione < 1) {
+//                    if (pastSteering-currentSteering>10){
+//                    duty_cycle = pastSteering+1;
+//                    }
+//                    else {
+//                        duty_cycle = currentSteering;
+//                    }
+//                    }
+//                } else if ((pastSteering - currentSteering) < 0) { //sterzo verso Dx
+//                    duty_cycle = duty_cycle + correzione;
+//                } else if ((pastSteering - currentSteering) > 0) { //sterzo verso Sx
+//                    duty_cycle = duty_cycle - correzione;
+//                }
+//            } else {
+//                duty_cycle = currentSteering;
+//            }
+////            if ((pastSteering - currentSteering) < 0) {
+////                duty_cycle = duty_cycle + correzione;
+////            }
+////            if ((pastSteering - currentSteering) > 0) {
+////                duty_cycle = duty_cycle - correzione;
+////            }
+//            pastSteering = duty_cycle;
+//            previousTimeCounter = timeCounter;
+//        }
         if (PORTCbits.RC0 == 0) {
             timer = (((duty_cycle * 700) / 90) + 800) *2;
             Ton = 65536 - timer;
@@ -201,16 +199,6 @@ void configurazione_iniziale(void) {
     LATE = 0x00;
     TRISE = 0xFF;
 
-    //debug sequence ------
-    PORTC = 0xff; //
-    delay_ms(100); //
-    PORTC = 0x00; //   
-    delay_ms(100); //   
-    PORTC = 0xff; //
-    delay_ms(100); //
-    PORTC = 0x00; //
-    delay_ms(100); //
-    //----------------------
     CANInitialize(4, 6, 5, 1, 3, CAN_CONFIG_LINE_FILTER_OFF & CAN_CONFIG_SAMPLE_ONCE & CAN_CONFIG_ALL_VALID_MSG & CAN_CONFIG_DBL_BUFFER_ON);
     RCONbits.IPEN = 1; //abilita priorità interrupt
 
@@ -234,11 +222,6 @@ void configurazione_iniziale(void) {
     INTCONbits.GIEH = 1; //abilita interrupt alta priorità
     INTCONbits.GIEL = 1; //abilita interrupt bassa priorità periferiche
 
-    //debug sequence ------    
-    PORTC = 0xff; //
-    delay_ms(100); //
-    PORTC = 0x00; //
-    //---------------------
 
     T0CON = 0x80; //imposta timer0, prescaler 1:2
 
