f76834eb3e69091e987ab494c54e5b4cf5295cb9
GianGian <gianlorenzomoser@alice.it>
2015-12-08 11:06:09 +0100

    aggiunto commenti

--- newmain.c
+++ newmain.c
@@ -1,10 +1,19 @@
 #define USE_AND_MASKS
-/* 
- * File:   newmain.c
- * Author: Simone
- *
- * Created on 3 dicembre 2015, 8.09
- */
+//==============================================================================
+// PROGRAMM : STERZO
+// WRITTEN BY : MOSER & RIGHETTI & CLEMENTI & GALVAGNI
+// DATA : 08/12/2015
+// VERSION : 1.0
+// FILE SAVED AS : STERZO.C
+// FOR PIC : 18F4480
+// CLOCK FREQUENCY : 16 MHZ
+// PROGRAMM FUNCTION: PROGRAMMA CHE GESTISCE IL SERVO DELLO STERZO
+//==============================================================================
+
+//==============================================================================
+//Dichiarazione librerie
+//==============================================================================
+
 #include <xc.h>
 #include "newfile.h"
 #include <CANlib.h>
@@ -16,6 +25,10 @@
 #include "math.h"
 #include "stdlib.h"
 
+//==============================================================================
+//Dichiarazione frequenza clock e codici centraline
+//==============================================================================
+
 #define _XTAL_FREQ 16000000
 #define Steering_info 0b10000000000000000000000000000
 #define Steering_change 0b00000000000000000000000000110
@@ -25,11 +38,13 @@ void configurazione_iniziale(void);
 void send_data(void);
 void calibrazione(void);
 CANmessage msg;
+
+//==============================================================================
+//Dichiarazione variabili
+//==============================================================================
+
+unsigned int limiteDx = 120; //da variare per limitare l'angolo di sterzata
 bit remote_frame = 0;
-
-//da variare per limitare l'angolo di sterzata
-unsigned int limiteDx = 120;
-
 char currentSteering = 180;
 char pastSteering = 1;
 char theorySteering = 0;
@@ -46,60 +61,65 @@ unsigned int Toff = 0;
 int errore = 0;
 int correzione = 0;
 int potenza = 2;
-BYTE counter_array [8] = 0;
+BYTE counter_array [8] = 0; //variabili per il CAN BUS
 BYTE currentSteering_array [8] = 0;
 BYTE data_array [8] = 0;
-//*************************************
-//ISR Alta priorità
-//*************************************
+
+//==============================================================================
+//ISR Alta priorità (creazione PWM a 50 Hz)
+//==============================================================================
 
 __interrupt(high_priority) void ISR_alta(void) {
     PORTCbits.RC0 = ~PORTCbits.RC0;
     T0CONbits.TMR0ON = 0;
-    if (PORTCbits.RC0 == 1) {
+    if (PORTCbits.RC0 == 1) { //Ton
         WriteTimer0(Ton);
         T0CONbits.TMR0ON = 1;
     }
-    if (PORTCbits.RC0 == 0) {
+    if (PORTCbits.RC0 == 0) { //Toff
         WriteTimer0(Toff);
         T0CONbits.TMR0ON = 1;
     }
 
-    INTCONbits.TMR0IF = 0;
+    INTCONbits.TMR0IF = 0; //reset flag
 }
-//*************************************
+//==============================================================================
 //ISR Bassa priorità (gestione can bus)
-//*************************************
+//==============================================================================
 
 __interrupt(low_priority) void ISR_bassa(void) {
-    if ((PIR3bits.RXB0IF == 1) || (PIR3bits.RXB1IF == 1)) {
-        if (CANisRxReady()) {
+    if ((PIR3bits.RXB0IF == 1) || (PIR3bits.RXB1IF == 1)) { //se arriva messaggio sul CAN BUS
+        if (CANisRxReady()) { 
             CANreceiveMessage(&msg);
             if (msg.RTR == 1) {
                 id = msg.identifier;
                 remote_frame = msg.RTR;
             }
-            if (msg.identifier == Steering_info) {
+            if (msg.identifier == Steering_info) { //richiesta informazioni
                 data_array[0] = theorySteering;
 
             }
-            if (msg.identifier == Steering_change) {
+            if (msg.identifier == Steering_change) { //cambio angolatura sterzo
                 //pastSteering = currentSteering;
                 theorySteering = msg.data[0];
                 currentSteering = theorySteering + calibration; //aggiunta calibrazione
                 currentSteering = (limiteDx * currentSteering) / 180;
             }
         }
-        PIR3bits.RXB0IF = 0;
+        PIR3bits.RXB0IF = 0; //reset flag
         PIR3bits.RXB1IF = 0;
     }
-    if (PIR2bits.TMR3IF) {
+    if (PIR2bits.TMR3IF) { //conteggio timer3 per contatore
         timeCounter++;
         TMR3H = 0x63;
         TMR3L = 0xC0;
         PIR2bits.TMR3IF = 0;
     }
 }
+
+//==============================================================================
+//Main
+//==============================================================================
 
 int main(void) {
     configurazione_iniziale();
@@ -155,6 +175,10 @@ int main(void) {
     //  }
 }
 
+//==============================================================================
+//Settaggi per invio dati sul CAN BUS
+//==============================================================================
+
 void send_data(void) {
     if (CANisTxReady()) {
         if (remote_frame == 1) {
@@ -170,6 +194,10 @@ void send_data(void) {
     }
 }
 
+//==============================================================================
+//Lettura tramite ADC del potenziometro (per calibrare lo sterzo)
+//==============================================================================
+
 void calibrazione(void) { //lettura ADC calibrazione
     ConvertADC();
     while (BusyADC());
@@ -177,25 +205,34 @@ void calibrazione(void) { //lettura ADC calibrazione
     calibration = (ADCResult - 511) / 45;
 }
 
+//==============================================================================
+//Configurazione bit
+//==============================================================================
+
 void configurazione_iniziale(void) {
 
-
     CANInitialize(4, 6, 5, 1, 3, CAN_CONFIG_LINE_FILTER_OFF & CAN_CONFIG_SAMPLE_ONCE & CAN_CONFIG_ALL_VALID_MSG & CAN_CONFIG_DBL_BUFFER_ON);
-    RCONbits.IPEN = 1; //abilita priorità interrupt
-
+        
+    //==========================================================================
     //azzeramento flag
+    //==========================================================================
     PIR3bits.RXB1IF = 0; //azzera flag interrupt can bus buffer1
     PIR3bits.RXB0IF = 0; //azzera flag interrupt can bus buffer0
     INTCONbits.TMR0IF = 0; //azzera flag timer0
     PIR2bits.TMR3IF = 0; //resetta flag interrupt timer 3
 
+    //==========================================================================
     //configurazione priorità
+    //==========================================================================
     IPR3bits.RXB1IP = 0; //interrupt bassa priorità per can
     IPR3bits.RXB0IP = 0; //interrupt bassa priorità per can
     INTCON2bits.TMR0IP = 1; //interrupt alta priorità timer0
     IPR2bits.TMR3IP = 0; //interrupt bassa priorità timer 3
 
+    //==========================================================================
     //Enable interrupts
+    //==========================================================================
+    RCONbits.IPEN = 1; //abilita priorità interrupt
     PIE3bits.RXB1IE = 1; //abilita interrupt ricezione can bus buffer1
     PIE3bits.RXB0IE = 1; //abilita interrupt ricezione can bus buffer0
     INTCONbits.TMR0IE = 1; //abilita interrupt timer0
@@ -203,16 +240,22 @@ void configurazione_iniziale(void) {
     INTCONbits.GIEH = 1; //abilita interrupt alta priorità
     INTCONbits.GIEL = 1; //abilita interrupt bassa priorità periferiche
 
-
+    //==========================================================================
+    //impostazione timer0 per PWM
+    //==========================================================================
     T0CON = 0x80; //imposta timer0, prescaler 1:2
 
+    //==========================================================================
     //impostazione timer3 per contatore (interrput ogni 10ms)
+    //==========================================================================
     TMR3H = 0x63;
     TMR3L = 0xC0;
     T3CON = 0x01; //abilita timer
     OpenADC(ADC_FOSC_16 & ADC_RIGHT_JUST & ADC_16_TAD, ADC_CH1 & ADC_REF_VDD_VSS & ADC_INT_OFF, ADC_2ANA); //re2
 
+    //==========================================================================
     //impostazione periodo timer prima volta
+    //==========================================================================
     timer = (((0 * 700) / 90) + 800) *2;
     Ton = 65536 - timer;
     Toff = 20000 - (timer / 2);
@@ -221,7 +264,9 @@ void configurazione_iniziale(void) {
     T0CONbits.TMR0ON = 1;
     Toff = 0x4588;
 
+    //==========================================================================
     //impostazione uscite/ingressi
+    //==========================================================================
     LATA = 0x00;
     TRISA = 0b11111100;
 
@@ -237,4 +282,3 @@ void configurazione_iniziale(void) {
     LATE = 0x00;
     TRISE = 0xFF;
 }
-
